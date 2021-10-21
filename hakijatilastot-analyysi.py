f9a25529ccecd3a107f2c54acf45fe074bcf9166
pirtnea <nea.pirttinen@cs.helsinki.fi>
2021-10-21 15:38:37 +0300

    Add histplot for age at intake

--- hakijatilastot-analyysi.py
+++ hakijatilastot-analyysi.py
@@ -254,10 +254,17 @@ print(students[['intake', 'age_at_intake', 'gender']].groupby(['intake', 'gender
 
 age_by_intake = students[['intake', 'age_at_intake']]
 
+print('Varmista kuvaajissa, että väylien labelit menevät oikein! Vain HYllä kolme sisääntuloväylää, muilla DEFA accept ja Main')
+
 # varmista hakuväylien järjestys kuvaajassa, ja siisti sen jälkeen kuvaajan selitys ottamalla kommentointi pois solun viimeisestä rivistä
 # labels-listan järjestystä saattaa joutua muuttamaan, jotta uudet labelit osuvat oikeisiin sisäänpääsyväylädatoihin
 g = sns.kdeplot(data = age_by_intake, x = 'age_at_intake', hue = 'intake', multiple = 'layer', bw_adjust = 0.5)
 g.set(xlim = (age_by_intake['age_at_intake'].min(), age_by_intake['age_at_intake'].max()), xlabel = 'Age at intake', ylabel = 'Distribution of ages')
+g.legend(title = 'Intake', labels = ['DEFA accept', 'Open uni.', 'Main'])
+plt.show()
+
+g = sns.histplot(data = age_by_intake, x = 'age_at_intake', hue = 'intake', multiple = 'layer', discrete = True)#, #bw_adjust = 0.5)
+g.set(xlim = (age_by_intake['age_at_intake'].min(), age_by_intake['age_at_intake'].max()))#, xlabel = 'Age at intake', ylabel = 'Distribution of ages')
 g.legend(title = 'Intake', labels = ['DEFA accept', 'Open uni.', 'Main'])
 plt.show()
 
